5d36fcc2a4d4893e15e1374c6657172927781ae0
Manuel Altalef <maltalef@auror.ar>
2021-08-16 16:41:54 -0300

    added nvim-tree config

---
+++ .config/nvim/lua/core/nvimtree.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+function M.setup()
+	local nvim_tree_config = require('nvim-tree.config')
+
+	for opt, val in pairs(mvim.builtin.nvimtree) do
+		vim.g['nvim_tree_' .. opt] = val
+	end
+
+	require('nvim-tree')
+end
+
+return M
