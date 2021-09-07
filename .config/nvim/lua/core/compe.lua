8c5e6d4d71905d07bb5d71efbfd765aa8ba860fb
Manuel Altalef <maltalef@auror.ar>
2021-09-07 16:38:46 -0300

    nvim:switched from vsnip to luasnip

--- .config/nvim/lua/core/compe.lua
+++ .config/nvim/lua/core/compe.lua
@@ -1,71 +1,73 @@
 vim.o.completeopt = "menuone,noselect"
+local Log = require('core.log')
 
-require('compe').setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 150;
-  documentation = true;
-  source = {
-    path = true;
-    buffer = true;
-    calc = true;
-    vsnip = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    spell = true;
-    tags = true;
-    snippets_nvim = true;
-    treesitter = true;
-  };
-}
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
+local function prequire(...)
+	local status_ok, lib = pcall(require, ...)
+	if not status_ok then
+		Log:get_default().error "Failed to load plugin compe"
+	else
+		return lib
+	end
 end
 
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif vim.fn.call("vsnip#available", {1}) == 1 then
-    return t "<Plug>(vsnip-expand-or-jump)"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
+local status_ok, compe = pcall(require, 'compe')
+if not status_ok then
+	Log:get_default().error "Failed to load plugin compe"
 end
 
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    return t "<Plug>(vsnip-jump-prev)"
-  else
-    -- If <S-Tab> is not working in your terminal, change it to <C-h>
-    return t "<S-Tab>"
-  end
+local M = {}
+
+local function t(str)
+	return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+local function check_back_space()
+	local col = vim.fn.col('.') - 1
+	if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+			return true
+	else
+			return false
+	end
+end
+
+function M.setup()
+	local compe = prequire('compe')
+	local luasnip = prequire('luasnip')
+	compe.setup(mvim.builtin.compe)
+
+	remap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+	remap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+	remap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+	remap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+	remap("i", "<C-E>", "<Plug>luasnip-next-choice", {})
+	remap("s", "<C-E>", "<Plug>luasnip-next-choice", {})
+
+	-- Use (s-)tab to:
+	--- move to prev/next item in completion menuone
+	--- jump to prev/next snippet's placeholder
+	_G.tab_complete = function()
+		if vim.fn.pumvisible() == 1 then
+			return t "<C-n>"
+		elseif luasnip and luasnip.expand_or_jumpable() then
+			return t "<Plug>luasnip-expand-or-jump"
+		elseif check_back_space() then
+			return t "<Tab>"
+		else
+			return vim.fn['compe#complete']()
+		end
+	end
+
+	_G.s_tab_complete = function()
+		if vim.fn.pumvisible() == 1 then
+			return t "<C-p>"
+		elseif luasnip and luasnip.jumpable(-1) then
+			return t "<Plug>luasnip-jump-prev"
+		else
+			-- If <S-Tab> is not working in your terminal, change it to <C-h>
+			return t "<S-Tab>"
+		end
+	end
+
+end
+
+return M
